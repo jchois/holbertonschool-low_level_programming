a55ae6e546e123972e168fecf68745349b5175f4
Juliana Chois <choisjuliana@gmail.com>
2020-02-05 21:01:36 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int main(void)
+/**
+* main - This is a print
+*/
+int main (void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
