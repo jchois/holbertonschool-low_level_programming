716d8a10535c6b304897688a901de7369c5fca13
Juliana Chois <choisjuliana@gmail.com>
2020-02-05 22:15:22 +0000

    changing info file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-* main - This function prints various types
-* Return: 0 always
-*/
+ * main - this is the main function
+ * Return: always return (0)
+ */
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long: %lu byte(s)\n", sizeof(long));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
