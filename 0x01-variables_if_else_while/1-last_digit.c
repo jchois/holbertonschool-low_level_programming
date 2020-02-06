dca007646f9b385d67217236fed921190326ba14
Juliana Chois <choisjuliana@gmail.com>
2020-02-06 19:52:41 +0000

    greater and less

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,13 +18,13 @@ if (lastDig > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, lastDig);
 }
-else if (lastDig < 6 && lastDig != 0)
-{
-printf("Last digit of %d is %d and less than 6 and not 0\n", n, lastDig);
-}
 else if (lastDig == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, lastDig);
+}
+else
+{
+printf("Last digit of %d is %d and less than 6 and not 0\n", n, lastDig);
 }
 return (0);
 }
