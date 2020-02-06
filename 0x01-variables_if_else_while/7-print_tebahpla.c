7a2f6436b8210e6b290ae6f6be118ef9be74bfe8
Juliana Chois <choisjuliana@gmail.com>
2020-02-06 22:39:18 +0000

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - print numbers
+* Return: 0
+*/
+int main(void)
+{
+int num;
+for (num = 122; num <= 97; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
