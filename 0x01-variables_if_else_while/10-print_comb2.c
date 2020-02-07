e06fd763c092574013ffe898d8654498ec1a2514
jchois <choisjuliana@gmail.com>
2020-02-07 04:55:22 +0000

    print the numbers from 00 to 99

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+/**
+* main - print the numbers from 00 to 99
+* Return: 0
+*/
 
 int main(void)
 {
+int num1, num2;
+
+for (num1 = 48; num1 < 58; num1++)
+{
+for (num2 = 48; num2 < 58; num2++)
+{
+putchar(num1);
+putchar(num2);
+if (num1 == 57 && num2 == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
 return (0);
 }
