b2f9a08993345f231fc54267b5897ce89694918e
Juliana Chois <choisjuliana@gmail.com>
2020-02-07 00:27:28 +0000

    init 9

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - print a random number
+* Return: 0 always
+*/
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+return (0);
+}
