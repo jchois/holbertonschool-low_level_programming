28c449eb22634eb01ad966e04bbdd18be8022bc2
jchois <choisjuliana@gmail.com>
2020-02-11 15:17:29 +0000

    fixing 2-print-alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,21 @@
 #include "holberton.h"
-
-void print_alphabet_x10(void){
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
 
 	int n;
+
 	char abc;
 
 	n = 0;
-	while (n <= 10){
+	while (n <= 10)
+	{
 		abc = 97;
-		while (abc <= 122){
+		while (abc <= 122)
+		{
 			_putchar(abc);
 			abc++;
 		}
