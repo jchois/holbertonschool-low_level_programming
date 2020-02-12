61254fd0890b722b117e96dd61bfb3e2a886d119
jchois <choisjuliana@gmail.com>
2020-02-12 06:11:16 +0000

    Adding 0x00-hello_world again

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+/**
+ * main - This is a print
+ * Return: return 0
+ */
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongint));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
