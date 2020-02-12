24653e618586a440578845ce456c6aed0c1988e3
jchois <choisjuliana@gmail.com>
2020-02-12 03:59:22 +0000

    testing positive and negative numbers

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
+
 /**
- * main - testing
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
 int main(void)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
