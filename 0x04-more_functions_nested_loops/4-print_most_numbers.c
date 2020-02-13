153d88e8b0c3cb1e5526a11a788729b147e08e72
jchois <choisjuliana@gmail.com>
2020-02-13 17:15:56 +0000

    fixing 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,13 @@
 
 void print_most_numbers(void)
 {
-	char i;
+	int i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != 50 && i != 52)
+		if (i != '2' && i != '4')
 		{
-			_putchar(i);
+			_putchar(i + '0');
 		}
 	}
 	_putchar('\n');
