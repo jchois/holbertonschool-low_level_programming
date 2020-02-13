57f5f92142b53531f55adf2b3b847f9b9ed75c4d
jchois <choisjuliana@gmail.com>
2020-02-13 17:10:03 +0000

    Fixing 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,16 @@ void more_numbers(void)
 {
 	int i, num;
 
-	i = 0;
-
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num > 10)
+			if (num > 9)
 			{
 				_putchar((num / 10) + '0');
 			}
 			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
-		i++;
 	}
 }
