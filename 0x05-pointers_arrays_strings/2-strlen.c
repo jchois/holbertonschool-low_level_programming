69dfe12d1e0b49755011b01a411f08513da08cf9
jchois <choisjuliana@gmail.com>
2020-02-18 05:41:28 +0000

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - length of string
+ * @s: string
+ * Return: returns length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	return (i);
+}
