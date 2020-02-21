41e1551e617d72944f353e02ce456f4d444a11fa
jchois <choisjuliana@gmail.com>
2020-02-21 04:52:14 +0000

    8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,10 +1,32 @@
 #include "holberton.h"
 
-/*
- *
- *
+/**
+ * rot13 - encode string
+ * @s: string
+ * Return: string
  */
-
-char *rot13(char *)
+char *rot13(char *s)
 {
+	int i;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			while ((s[i] >= 'A' && s[i] < 'N') || (s[i] >= 'a' && s[i] < 'n'))
+			{
+				s[i] += 13;
+				i++;
+			}
+			while ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
+			{
+				s[i] -= 13;
+				i++;
+			}
+		}
+		else
+			i++;
+	}
+	return (s);
 }
