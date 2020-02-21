1c3089055286cc95bb65993e6f7e353c7a6b1c27
jchois <choisjuliana@gmail.com>
2020-02-21 23:39:17 +0000

    function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,32 +1,31 @@
 #include "holberton.h"
 
 /**
- * rot13 - encode string
- * @s: string
- * Return: string
+ * rot13 - encodes a string using rot13
+ * @s: string to be encoded
+ * Return: pointer to encoded string
  */
+
 char *rot13(char *s)
 {
-	int i;
+	int i = 0;
+	int base;
 
-	i = 0;
-	while (s[i] != 0)
+	while (s[i] != '\0')
 	{
-		if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))
+
+		if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 90))
 		{
-			while ((s[i] >= 'A' && s[i] < 'N') || (s[i] >= 'a' && s[i] < 'n'))
-			{
-				s[i] += 13;
-				i++;
+			base = 'a';
+
+			while (s[i] >= 'A' && s[i]  <= 'Z')
+			{	base = 'A';
+				break;
 			}
-			while ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
-			{
-				s[i] -= 13;
-				i++;
-			}
+			s[i] = ((((s[i] - base) + 13) % 26) + base);
 		}
-		else
-			i++;
+		i++;
 	}
 	return (s);
 }
+
