509ffec619538725f49ad14b987f8d40bdee35a0
jchois <choisjuliana@gmail.com>
2020-02-25 02:45:41 +0000

    memcpywq

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include  "holberton.h"
+
+/**
+ * _memcpy - function that copies memory area
+ * @dest: destination
+ * @src: source
+ * @n: bytes to copy
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i < n ; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
+
