496e436b4276fd7482d168e5f249fe0e99ddcd87
jchois <choisjuliana@gmail.com>
2020-02-25 01:57:06 +0000

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strcat - function that fills memory with a constant byte
+ * @s: Memory space to be filled
+ * @b: The character to fill the memory area
+ * @n: Unsigned parameter
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
