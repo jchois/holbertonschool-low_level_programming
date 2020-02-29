11cf55e5c202d0846e082ae23f0e1794bffbe479
jchois <choisjuliana@gmail.com>
2020-02-29 20:07:47 +0000

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print its name, followed by a new line
+ * @argc: asd
+ * @argv: asd
+ * Return: 0
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
