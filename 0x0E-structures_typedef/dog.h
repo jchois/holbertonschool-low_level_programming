80066b9fab5346a9b26a4d159277dc7780bbcde8
jchois <choisjuliana@gmail.com>
2020-03-09 21:41:58 +0000

    Django task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - define a dog struct
+ * @name: char
+ * @age: float
+ * @owner: char
+ *
+ * Description: struct to define a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
