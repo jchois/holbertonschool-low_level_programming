1aa205e679ab870199f9cb589dadf575b1f040ee
jchois <choisjuliana@gmail.com>
2020-03-10 22:17:00 +0000

    Create a new structure - task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,14 +1,59 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strcpy - copy a string
+ * @d: destination
+ * @s: source
+ * Return: d
+ */
+
+char *strcpy(char *d, char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		d[i] = s[i];
+
+	d[i] = '\0';
+	return (d);
+}
+
 /**
  * new_dog - creates a new dog structure
  * @name: char var
  * @age: float
  * @owner: char
+ * Return: new ptr
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *new;
 
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+		return (NULL);
+
+	(*new).name = malloc(sizeof(name) + 1);
+	if (name == NULL)
+	{
+		free(name);
+		return (NULL);
+	}
+
+	(*new).owner = malloc(sizeof(owner) + 1);
+	if (owner == NULL)
+	{
+		free(owner);
+		return (NULL);
+	}
+
+	strcpy((*new).name, name);
+	strcpy((*new).owner, owner);
+	(*new).age = age;
+
+	return (new);
 }
 
