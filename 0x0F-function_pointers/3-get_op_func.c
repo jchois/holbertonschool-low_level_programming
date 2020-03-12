5402651c83f4db2e126100ed4d128a26a9302767
jchois <choisjuliana@gmail.com>
2020-03-12 04:07:36 +0000

    adding some things about task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+
+/*
+ * get_op_func -points a function to another function
+ * @s: the operator
+ * Return: ptr
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (*(ops[i].op) == *s)
+		{
+			return (ops[i].f);
+		}
+	}
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/*
+ * op_add - sum
+ * @a: var
+ * @b: var
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub: sub
+ * @a: var
+ * @b: var
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul: mul
+ * @a: var
+ * @b: var
+ * Return: mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div: div
+ * @a: var
+ * @b: var
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod: mod
+ * @a: var
+ * @b: var
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
