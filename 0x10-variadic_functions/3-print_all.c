9d54cd3f52c1618c514e76fb29665714e8361801
jchois <choisjuliana@gmail.com>
2020-03-14 21:02:06 +0000

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 
+
 /**
  * print_all - prints anything
  *
@@ -8,10 +9,80 @@
 
 void print_all(const char * const format, ...)
 {
-	struct **p = {
-		{"c", char},
-		{"i", int},
-		{"f", float},
-		{"s", char *}
+	va_list list;
+	int i, j;
+	char *ptr;
+
+	Dtype prt[] = {
+		{"c", print_s},
+		{"i", print_i},
+		{"f", print_f},
+		{"s", print_s},
 	};
+
+	va_star(list, format);
+
+	i = 0;
+	while (format != NULL && *(format + i) != '\0')
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if(ptr[j].type[0] == format[i])
+			{
+				ptr[j].f(list);
+			}
+			j++;
+		}
+		i++;
+	}
+
 }
+
+/**
+ * print_c - print char
+ * @c: char
+ * Return: Nothing
+ */
+void print_c(va_list c)
+{
+
+	printf("%s", va_arg(c, char));
+}
+
+/**
+ * print_i - print int
+ * @i: int
+ * Return: Nothing
+ */
+void print_i(va_list i)
+{
+	printf("%i", va_arg(i, int));
+}
+
+/**
+ *  print_f - print float
+ * @f: float
+ * Return: Nothing
+ */
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_s - s
+ * @s: string
+ * Return: nothing
+ */
+void print_s(va_list s)
+{
+	char *x;
+
+	x = va_arg(x, char *);
+	if (x == NULL)
+		printf("(nil)");
+
+	printf("%s", x);
+}
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,8 +3,16 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+typedef struct
+{
+	char type;
+	int (*f)();
+} Dtype;
+
 #endif
