904e95d19ae5e9d8bec26c928db89b05fd2924c4
jchois <choisjuliana@gmail.com>
2020-03-21 00:50:05 +0000

    fix betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@ void __attribute__((constructor)) before_main();
  * before_main - prints something before main
  * Return: nothing
  */
-void before_main()
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
