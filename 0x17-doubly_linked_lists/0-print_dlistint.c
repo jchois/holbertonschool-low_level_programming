3877e53f0dbace412289322003c9573fad66e51f
jchois <choisjuliana@gmail.com>
2020-05-02 20:47:30 +0000

    betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,7 +8,8 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i = 0;
+	size_t i;
+
 	if (h == NULL)
 		return (0);
 
@@ -17,5 +18,7 @@ size_t print_dlistint(const dlistint_t *h)
 		printf("%d\n", h->n);
 		h = h->next;
 	}
+
 	return (i);
 }
+
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,9 +9,10 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i;
+
 	for (i = 0; h != NULL; i++)
 	{
 		h = h->next;
 	}
-	return(i);
+	return (i);
 }
