d707a711ec21c18c9f0e3e6e2fc4328d570dbadc
jchois <choisjuliana@gmail.com>
2020-05-02 18:38:22 +0000

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/*
+ * dlistint_len - elements in a linked list
+ * @h: head
+ * Return: elements in a linked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+	for (i = 0; h != NULL; i++)
+	{
+		h = h->next;
+	}
+	return(i);
+}
