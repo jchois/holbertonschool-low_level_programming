8c37090ecd9f5cbe4c1560c0c04c619cdce4f94e
jchois <choisjuliana@gmail.com>
2020-05-29 20:32:35 -0500

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,26 +7,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *hash_t = ht;
-	hash_node_t *n, *aux;
 	unsigned long int i;
+	hash_node_t *next;
 
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+		while (ht->array[i])
 		{
-			n = ht->array[i];
-			while (n != NULL)
-			{
-				aux = n->next;
-				free(n->key);
-				free(n->value);
-				free(n);
-				n = aux;
-			}
+			next = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = next;
 		}
 	}
-	free(hash_t->array);
-	free(hash_t);
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
 }
 
